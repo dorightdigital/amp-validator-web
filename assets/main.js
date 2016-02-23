7ee8fa302e8fd0eb9fad6c6f03043ce8d4435cd6
Mat Carey <mat@dorightdigital.com>
2016-02-23 23:49:42 +0000

    Adding advert.

--- assets/main.js
+++ assets/main.js
@@ -9,3 +9,17 @@ $(function () {
     }
   });
 });
+
+if (false) {
+var $ad = $('<ins>').addClass('adsbygoogle').addClass('sideBanner').attr({
+  'data-ad-client': 'ca-pub-5738874532953979',
+  'data-ad-slot': '2316934440'
+}).prependTo($('body'));
+
+if ($ad.is(':visible')) {
+  (adsbygoogle = window.adsbygoogle || []).push({});
+  $('body').addClass('with-sidebar');
+} else {
+  $ad.remove();
+}
+}
